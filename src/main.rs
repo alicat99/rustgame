1a0c69441017e43e26942b0c6922dd8b780a4a4e
alicat9912374@gmail.com <alicat9912374@gmail.com>
2023-10-31 11:38:56 +0900

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-//! Displays a single [`Sprite`], created from an image.
-
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .run();
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("branding/icon.png"),
-        ..default()
-    });
 }
